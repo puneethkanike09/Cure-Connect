ec7146af5ba9bc2ff8f3e302cc77b0efee4f37d8
Puneeth K <kanike.puneeth@gmail.com>
2024-10-17 15:21:09 +0530

    created the admin login api with generating the token, and we created the middlware for the admin where the admin add the doctor, if the admin want to add the doctor then he should have to login to get the token, if the token matches in the header then only he would able to add the doctor

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctor.model.js";
+import jwt from "jsonwebtoken";
 
 
 //Api for adding doctors
@@ -63,5 +64,24 @@ const addDoctor = async (req, res) => {
     }
 }
 
-export {addDoctor}
+
+//API for admin login
+const loginAdmin = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password, process.env.JWT_SECRET)
+            res.json({success: true, message: "Login Successful", token})
+        }else{
+            res.json({success: false, message: "Invalid Credentials"})
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+}
+
+
+export {addDoctor, loginAdmin}
 
--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { addDoctor } from '../controllers/admin.controller.js';
+import { addDoctor, loginAdmin } from '../controllers/admin.controller.js';
 import upload from '../middlewares/multer.js';
+import authAdmin from '../middlewares/authAdmin.js';
 
 const adminRouter = express.Router();
 
 // add doctor
-adminRouter.post('/add-doctor', upload.single('image'), addDoctor);
+adminRouter.post('/add-doctor',authAdmin,upload.single('image'), addDoctor);
+adminRouter.post('/login', loginAdmin);
 
 export default adminRouter;
