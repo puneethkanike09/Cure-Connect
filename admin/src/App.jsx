317635b31260ff461e0fd9febe52a6504e9ba58b
Puneeth K <kanike.puneeth@gmail.com>
2024-10-17 17:02:31 +0530

    added the react tostify, succuessfully integrated the admin login api and handled the token.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,21 @@
+import { useContext } from "react";
 import Login from "./pages/Login"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { AdminContext } from "./context/AdminContext";
 
 const App = () => {
-  return (
-    <div>
-      <Login />
+  const{aToken} = useContext(AdminContext)
+  return aToken ? (
+    <div>    
+      <ToastContainer/>
     </div>
+  ) : (
+    <>
+    <Login />
+      <ToastContainer/>
+    </>
   )
 }
 
--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,20 +1,49 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import {assets} from '../assets/assets'
+import { AdminContext } from '../context/AdminContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 
 const Login = () => {
   const [state, setState] = useState('Admin')
+  const[email, setEmail] = useState('')
+  const[password, setPassword] = useState('')
+  const {setAToken, backendUrl} = useContext(AdminContext)
+
+  const onSubmitHandler =  async (e)=>{
+      e.preventDefault();
+      try {
+        if(state=== 'Admin'){
+          const {data} = await axios.post(backendUrl+'/api/admin/login', {email, password})
+          if(data.success){
+            toast.success(data.message)
+            localStorage.setItem('aToken', data.token)
+            setAToken(data.token)
+          }else{
+            toast.error(data.message)
+          }
+        }else{
+
+        }
+      } catch (error) {
+        console.log(error)
+      }
+      
+  }
+  
+
   return (
-    <form className='min-h-[80vh] flex items-center'>
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
       <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-[#5e5e5e] text-sm shadow-lg'>
         <p className='text-2xl font-semibold m-auto'><span className='text-primary'>{state}</span> Login</p>
         <div className='w-full'>
           <p>Email</p>
-          <input className='border border-[#dadada] rounded w-full p-2 mt-1' type="email" required />
+          <input onChange={(e)=> setEmail(e.target.value)} value={email} className='border border-[#dadada] rounded w-full p-2 mt-1' type="email" required />
         </div>
         <div className='w-full'>
           <p>Password</p>
-          <input className='border border-[#dadada] rounded w-full p-2 mt-1' type="password" required />
+          <input onChange={(e)=> setPassword(e.target.value)} value={password} className='border border-[#dadada] rounded w-full p-2 mt-1' type="password" required />
         </div>
         <button className='bg-primary text-white rounded-md w-full py-2 text-base'>Login</button>
         {
