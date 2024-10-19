06208457f230495ce84b261b43aae690811d9bc2
Puneeth K <kanike.puneeth@gmail.com>
2024-10-19 15:01:20 +0530

    Created the controller to change the availability for the doctors, I added this in the doctor controller because I am hoing to use this route later for the doctor side also to change the availabilty.

---
+++ backend/controllers/doctor.controller.js
@@ -0,0 +1,15 @@
+import doctorModel from "../models/doctor.model.js"
+
+const changeAvailability = async (req, res) => {
+    try {
+        const {docId} = req.body
+        const docData = await doctorModel.findById(docId)
+        await doctorModel.findByIdAndUpdate(docId, {available: !docData.available})
+        res.json({success: true, message: "Availability changed successfully"})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+}
+
+export {changeAvailability}
