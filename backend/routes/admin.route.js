53786ecc9823ddd1f1915ff8002b678f0bcac298
Puneeth K <kanike.puneeth@gmail.com>
2024-10-19 14:20:41 +0530

    Created the API for the backend to get all the doctor details so that we can show them in the admin panel

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addDoctor, loginAdmin } from '../controllers/admin.controller.js';
+import { addDoctor, allDoctors, loginAdmin } from '../controllers/admin.controller.js';
 import upload from '../middlewares/multer.js';
 import authAdmin from '../middlewares/authAdmin.js';
 
@@ -8,5 +8,6 @@ const adminRouter = express.Router();
 // add doctor
 adminRouter.post('/add-doctor',authAdmin,upload.single('image'), addDoctor);
 adminRouter.post('/login', loginAdmin);
+adminRouter.post('/all-doctors',authAdmin, allDoctors);
 
 export default adminRouter;
