aa0eb999fbfa9a6a8b9430b963aab21527930301
Puneeth K <kanike.puneeth@gmail.com>
2024-10-20 19:46:36 +0530

    Integrated the getProfile and updateProfile APIs to the frontend, and the functionality is working as expected

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -9,14 +9,16 @@ const AppContextProvider = (props) => {
   const currencySymbol = "₹";
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [doctors, setDoctors] = useState([]);
-  const [token, setToken] = useState(localStorage.getItem("token") ? localStorage.getItem("token") : false ) //if we dont set the localstorage then when we refresh the user will get logout
-
+  const [token, setToken] = useState(
+    localStorage.getItem("token") ? localStorage.getItem("token") : false
+  ); //if we dont set the localstorage then when we refresh the user will get logout
+  const [userData, setUserData] = useState(false);
+  
   const getDoctorsData = async () => {
     try {
       const { data } = await axios.get(backendUrl + "/api/doctor/list");
       if (data.success) {
         setDoctors(data.doctors);
-        
       } else {
         toast.error(data.message);
       }
@@ -26,16 +28,44 @@ const AppContextProvider = (props) => {
     }
   };
 
+  const loadUserProfileData = async () => {
+  try {
+    const { data } = await axios.get(backendUrl + "/api/user/get-profile", {
+      headers: { token },
+    });
+    if (data.success) {
+      setUserData(data.userData); // Access userData instead of user
+    } else {
+      toast.error(data.message);
+    }
+  } catch (error) {
+    console.log(error);
+    toast.error(error.message);
+  }
+};
+
+
   useEffect(() => {
     getDoctorsData();
   }, []);
+
+  useEffect(() => {
+    if (token) {
+      loadUserProfileData();
+    } else {
+      setUserData(false);
+    }
+  }, [token]);
 
   const value = {
     doctors,
     currencySymbol,
     token,
     setToken,
-    backendUrl
+    backendUrl,
+    userData,
+    setUserData,
+    loadUserProfileData,
   };
 
   return (
