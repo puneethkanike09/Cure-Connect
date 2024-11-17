de1a30e36b7db33f3d569d0c557c9f043fee89e1
Puneeth <reachout.puneeth@gmail.com>
2024-11-17 10:51:20 +0530

    Able to integrate the API to display the appointments of the users in the admin side

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -7,6 +7,7 @@ export const AdminContext = createContext()
 const AdminContextProvider = (props) => {
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '' )
     const [doctors, setDoctors] = useState([])
+    const [appointments, setAppointments] = useState([])
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const getAllDoctors = async () => {
@@ -41,11 +42,27 @@ const AdminContextProvider = (props) => {
         }
     }
 
+    const getAllAppointments = async () => {
+        try {
+            const {data} = await axios.get(backendUrl+'/api/admin/appointments', {headers:{aToken}})
+            if(data.success){
+                setAppointments(data.appointments)
+                console.log(data.appointments)
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
     const  value = {
         aToken, setAToken,
         backendUrl,
         doctors, getAllDoctors,
-        changeAvailability
+        changeAvailability,
+        appointments, setAppointments, getAllAppointments
     }
 
     return (
