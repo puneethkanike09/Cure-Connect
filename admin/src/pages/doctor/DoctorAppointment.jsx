e53b38db64bfaf00e117b9d16930efed4b6003c2
Puneeth <reachout.puneeth@gmail.com>
2024-11-18 20:39:41 +0530

    API working as expected

--- admin/src/pages/doctor/DoctorAppointment.jsx
+++ admin/src/pages/doctor/DoctorAppointment.jsx
@@ -1,5 +1,13 @@
+import { useContext, useEffect } from "react"
+import { DoctorContext } from "../../context/DoctorContext"
 
 const DoctorAppointment = () => {
+  const {dToken, appointments, getAppointments} = useContext(DoctorContext)
+  useEffect(()=>{
+    if(dToken){
+      getAppointments()
+    }
+  },[dToken])
   return (
     <div>
       Doctor appintment
