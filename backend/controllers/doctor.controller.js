9f3c884f827d32ad9f53f80829805b6cd2e4cbf6
Puneeth <reachout.puneeth@gmail.com>
2024-11-19 10:29:08 +0530

    Created the API for the Doctor to get the Dashboard details

--- backend/controllers/doctor.controller.js
+++ backend/controllers/doctor.controller.js
@@ -126,4 +126,37 @@ const appointmentCancel = async (req, res) => {
 }
 
 
-export {changeAvailability, doctorList, adminDashboard, loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel}
+//API to get dashboard  data for Doctor Panel
+const doctorDashboard = async (req, res) => {
+    try {
+        const {docId} = req.body
+        const appointments = await appointmentModel.find({docId})
+        let earlings = 0
+        appointments.map((item)=>{
+            if(item.isCompleted || item.payment){
+                earlings += item.amount
+            }
+        })
+        let patients = []
+        appointments.map((item)=>{
+            if(!patients.includes(item.userId)){
+                patients.push(item.userId)
+            }
+        })
+
+        const dashData = {
+            earlings,
+            appointments: appointments.length,
+            patients: patients.length,
+            latestAppointments: appointments.reverse().slice(0,5)
+        }
+
+        res.json({success: true, dashData})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+}
+
+
+export {changeAvailability, doctorList, adminDashboard, loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel, doctorDashboard}
